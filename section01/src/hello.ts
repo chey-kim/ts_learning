2973d0ad84817efbf4296f893a68c9e549250917
seop <77163842+seop-kim@users.noreply.github.com>
2025-06-04 00:43:57 +0900

    chore: Update tsconfig and refactor sample files
    - `tsconfig.json` 파일을 추가하여 TypeScript 컴파일 옵션을 구성하였습니다.
    - `index.ts`와 `hello.ts` 파일을 수정하여 코드 예제를 리팩토링하였습니다.
    - `.gitignore`에 JavaScript 파일을 제외하도록 설정을 업데이트하였습니다.

---
+++ section01/src/hello.ts
@@ -0,0 +1,5 @@
+// export const hello = (msg: string) => {
+//     console.log('Hello World2' + msg);
+// }
+
+const a = 1;
--- section01/src/index.ts
+++ section01/src/index.ts
@@ -1,2 +1,13 @@
-console.log('Hello World');
-const a: number = 1;
+// import {hello} from './hello';
+// hello('qwdqwd');
+
+// const func = () => {
+//     console.log('Hello World');
+// }
+
+// ts 는 파일 내 export 가 없을 경우 해당 파일은 전역에서 처리되는 것으로 판단하여
+// 다른 파일에 동일한 변수명이 동일한 스코프에 존재하는 경우 에러를 발생시킨다.
+// moduleDetection를 tsconfig.json 에서 force로 설정하면 해당 에러는 발생하지 않는다.
+// moduleDetection : force는 컴파일시 파일에 export {}; 를 자동생성한다.
+
+const a = 1;
