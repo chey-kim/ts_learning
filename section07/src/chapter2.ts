d0c065b520db56608379042e7cf4277820a4a653
seop <77163842+seop-kim@users.noreply.github.com>
2025-06-12 00:13:22 +0900

    chore: Add chapter2.ts with map and forEach examples
    - `map` 함수와 `forEach` 함수의 구현 및 활용 사례를 작성하였습니다.
    - 제네릭 타입을 활용하여 함수의 범용성을 높였습니다.
    - 배열 데이터를 처리하고 변환하는 방법을 설명하였습니다.

---
+++ section07/src/chapter2.ts
@@ -0,0 +1,44 @@
+// map
+const arr = [1, 2, 3];
+const newArr: number[] = arr.map((item: number) => {
+    return item * 2;
+});
+
+
+function map<T, U>(arr: T[], callback: (item: T) => U) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(callback(arr[i]));
+    }
+
+    return result;
+}
+
+map([1, 2, 3], (item) => {
+    return item * 2;
+});
+
+let map1 = map(['hi', 'hello', 'hey'], (item) => parseInt(item));
+
+
+// forEach
+
+const arr2 = [1, 2, 3];
+// arr2.forEach((item) => {
+//     console.log(item);
+// });
+
+function forEach<T>(arr: T[], callback: (item: T) => void) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i])
+    }
+}
+
+forEach(arr2, (item) => {
+        console.log(item.toFixed());
+    }
+)
+
+forEach(['123', '456'], (item) => {
+    console.log(item.toUpperCase());
+});
