acab58e97b5cd8c27c92ed2ed82eceae08ec389a
seop <77163842+chey-kim@users.noreply.github.com>
2025-06-22 23:13:57 +0900

    chore: Add Record type examples in chapter1.ts
    - `Record` 유틸리티 타입과 이를 커스터마이징한 `CustomRecord` 타입에 대한 예제를 추가하였습니다.
    - 기존 타입 코드에 불필요한 공백(화이트스페이스)을 정리하였습니다.
    - 코드 가독성을 향상시키기 위해 부분적으로 포맷을 수정하였습니다.

--- section09/src/chapter1.ts
+++ section09/src/chapter1.ts
@@ -1,7 +1,7 @@
 // 분산적인 조건부 타입
 type StringNumberSwitch<T> = [T] extends [number] ? string : number;
-let a : StringNumberSwitch<number>;
-let b : StringNumberSwitch<string>;
+let a: StringNumberSwitch<number>;
+let b: StringNumberSwitch<string>;
 let c: StringNumberSwitch<number | string>;
 
 let d: StringNumberSwitch<boolean | number | string>;
@@ -25,3 +25,28 @@ type A = Exclude<number | string | boolean, string>;
 type Extract<T, U> = T extends U ? T : never;
 type B = Extract<number | string | boolean, string>;
 
+
+// Record<K, V>
+type ThumbnailLegacy = {
+    large: {
+        url: string;
+    },
+    medium: {
+        url: string;
+    },
+    small: {
+        url: string;
+    },
+    watch: {
+        url: string;
+    }
+}
+
+type CustomRecord<K extends keyof any, V> = {
+    [key in K] : V;
+}
+
+type Thumbnail = Record<'large' | 'medium' | 'small' | 'watch', {
+    url: string;
+    size: number;
+}>
