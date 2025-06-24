be88bbe8d22b54d2680d45344375c2c38b77e609
seop <77163842+chey-kim@users.noreply.github.com>
2025-06-24 23:50:24 +0900

    feat: Add reducer for TODO management and TodoItem component
    - `useReducer`를 활용하여 TODO 상태 관리 로직을 구현하였습니다.
    - `TodoItem` 컴포넌트를 생성하여 개별 TODO 항목을 렌더링하고 삭제 기능을 추가하였습니다.
    - `types.ts` 파일을 추가하여 Todo 인터페이스를 분리하고 모듈화하였습니다.

---
+++ section11/src/components/TodoItem.tsx
@@ -0,0 +1,18 @@
+import {Todo} from "../types";
+
+interface Props extends Todo {
+    onClickDelete: (id: number) => void;
+}
+
+export default function TodoItem(props: Props) {
+    const onClickDelete = () => {
+        props.onClickDelete(props.id);
+    }
+
+    return (
+        <div>
+            {props.id}번 : {props.content}
+            <button onClick={onClickDelete}>Delete</button>
+        </div>
+    )
+}
