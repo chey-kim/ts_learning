077ab4f00ff290d7125ed868c1eaae8b4377b576
seop <77163842+chey-kim@users.noreply.github.com>
2025-06-24 23:33:45 +0900

    feat: Add TODO application with basic components
    - TODO 애플리케이션의 초기 구조를 구현하였습니다.
    - `App.tsx`에서 상태 관리를 추가하고 `Editor` 컴포넌트를 생성하여 텍스트 입력 및 추가 기능을 구현하였습니다.
    - `Editor.tsx`는 입력 필드 및 추가 버튼을 포함하며, 부모 컴포넌트로 데이터를 전달합니다.

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css';
+import React, {useRef, useState} from "react";
+import Editor from "./components/Editor";
+
+interface Todo {
+    id: number;
+    content: string;
+}
 
 function App() {
-  return (
-      <div className="App"></div>
-  );
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const idRef = useRef(0);
+
+    const onClickAdd = (text: string) => {
+        setTodos([...todos, {id: idRef.current++, content: text}]);
+    }
+
+    return (
+        <div className="App">
+            <h1>TODO</h1>
+            <Editor onClickAdd={onClickAdd}></Editor>
+        </div>
+    );
 }
 
 export default App;
